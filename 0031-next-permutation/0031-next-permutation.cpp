a568b1096b12d043fdbedf05fa9a8acbe95e3c4b
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2024-07-10 23:33:33 +0530

    Time: 4 ms (55.29%), Space: 14.5 MB (54.76%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+        
+        // Find the first pair from right where nums[i] < nums[i+1]
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        
+        if (i >= 0) {
+            int j = n - 1;
+            // Find the smallest number on the right side of i that is greater than nums[i]
+            while (j > i && nums[j] <= nums[i]) {
+                j--;
+            }
+            // Swap nums[i] and nums[j]
+            swap(nums[i], nums[j]);
+        }
+        
+        // Reverse the subarray to the right of i
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
