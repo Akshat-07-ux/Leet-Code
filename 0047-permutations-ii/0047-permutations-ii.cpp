da3f91bb324e846adba2adde244dee69e5073a28
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2024-07-17 19:27:50 +0530

    Time: 4 ms (76.04%), Space: 14.6 MB (17.69%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        backtrack(nums, 0, result);
+        return result;
+    }
+
+private:
+    void backtrack(vector<int>& nums, int start, vector<vector<int>>& result) {
+        if (start == nums.size()) {
+            result.push_back(nums);
+            return;
+        }
+
+        unordered_set<int> used;
+        for (int i = start; i < nums.size(); i++) {
+            if (used.find(nums[i]) != used.end()) continue;
+            used.insert(nums[i]);
+            
+            swap(nums[start], nums[i]);
+            backtrack(nums, start + 1, result);
+            swap(nums[start], nums[i]);
+        }
+    }
+};
