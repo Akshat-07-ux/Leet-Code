be1c5af47c49f534593a0df62ce299ff6306b6fc
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2025-03-15 20:46:24 +0530

    Time: 27 ms, Memory: 60.9 MB - LeetHub

---
+++ 1398-64-2560-house-robber-iv/1398-64-2560-house-robber-iv.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canRob(vector<int>& nums, int k, int maxCap) {
+        int count = 0;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= maxCap) {
+                count++;
+                i++; // Skip the adjacent house
+            }
+            if (count >= k) return true;
+        }
+        
+        return false;
+    }
+
+    int minCapability(vector<int>& nums, int k) {
+        int left = *min_element(nums.begin(), nums.end());
+        int right = *max_element(nums.begin(), nums.end());
+        int ans = right;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (canRob(nums, k, mid)) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+};
